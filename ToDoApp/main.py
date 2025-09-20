d2f4c307709a39d11e2fd92a37a7de254e146fbb
maxymfarenyk <maxfar2004@gmail.com>
2025-09-20 20:31:29 +0300

    Changed path to static files

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-app.mount("/static", StaticFiles(directory="TodoApp/static"), name="static")
+app.mount("/static", StaticFiles(directory="ToDoApp/static"), name="static")
 
 
 @app.get("/")
