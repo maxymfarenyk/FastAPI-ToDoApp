d4ca655f577d3ae6131ef600a26c0618285eb9cb
maxymfarenyk <maxfar2004@gmail.com>
2025-09-21 17:48:08 +0300

    Added missing )

--- ToDoApp/static/js/base.js
+++ ToDoApp/static/js/base.js
@@ -28,6 +28,7 @@ function logout() {
             document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
             }
         }
+    )
     });
     window.location.href = '/auth/login-page';
 }
